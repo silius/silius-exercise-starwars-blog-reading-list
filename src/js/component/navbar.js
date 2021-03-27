583a2b72b165746271e54ee11d3dde83277d2a43
Adrian Acosta <aacrealms@hotmail.com>
2021-03-27 17:01:14 +0000

    Includes react bootstrap

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../img/star_wars_logo_PNG39.png";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				{/* <span className="navbar-brand mb-0 h1"></span> */}
+				<img src={logo} alt="" style={{ width: "90px", height: "90px" }} />
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
